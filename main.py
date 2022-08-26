72bdf73926e94096e8c3aa44b608a24034240bf9
anooppandikashala <anooppandikashala@gmail.com>
2022-08-26 17:53:45 +0530

    functions added

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 awsServices = ["s3", "ec2"]
 azureServices = ["blob", "collectors"]
 gcpServices = ["disk", "compute", "policies"]
-
 availableAuditCloudServices: dict = {"aws": awsServices, "azure": azureServices, "gcp": gcpServices}
 
 
@@ -48,20 +47,61 @@ def printSecondaryMenu(menuList: list):
 
 
 def addKeyToDatabase(keyPath):
+    # Do your code here
     pass
 
 
 def getKeyFilePath():
     defaultKeyPath = "/Modules/Database/"
     path = input("Enter the key-file full path")
-    if str(path).lower() != defaultKeyPath.lower() and defaultKeyPath.lower() not in str(path).lower() and str(path).lower() not in defaultKeyPath.lower() :
+    if str(path).lower() != defaultKeyPath.lower() and defaultKeyPath.lower() not in str(path).lower() and str(
+            path).lower() not in defaultKeyPath.lower():
         res = input("Do you want to add the key to the database? (y/n)")
         if str(res).lower() == "y":
             addKeyToDatabase(path)
     return path
 
 
-def printMenu():
+def doAWSService(selectedServiceInstance):
+    if selectedServiceInstance.lower() == "s3":
+        print("Doing AWS S3 Audit.....!")
+        # Do your code here
+    elif selectedServiceInstance.lower() == "ec2":
+        print("Doing AWS EC2 Audit.....!")
+        # Do your code here
+
+
+def doAzureService(selectedServiceInstance):
+    if selectedServiceInstance.lower() == "blob":
+        print("Doing Azure Blob Audit.....!")
+        # Do your code here
+    elif selectedServiceInstance.lower() == "collectors":
+        print("Doing Azure Collectors Audit.....!")
+        # Do your code here
+
+
+def doGCPService(selectedServiceInstance):
+    if selectedServiceInstance.lower() == "disk":
+        print("Doing GCP Disk Audit.....!")
+        # Do your code here
+    elif selectedServiceInstance.lower() == "compute":
+        print("Doing GCP Compute Audit.....!")
+        # Do your code here
+    elif selectedServiceInstance.lower() == "policies":
+        print("Doing GCP Policies Audit.....!")
+        # Do your code here
+
+
+def doAudit(selectedServiceInstance: str):
+    if selectedServiceInstance.lower() in awsServices:
+        doAWSService(selectedServiceInstance)
+    elif selectedServiceInstance.lower() in azureServices:
+        doAzureService(selectedServiceInstance)
+    elif selectedServiceInstance.lower() in gcpServices:
+        doGCPService(selectedServiceInstance)
+
+
+def startCloudAudit():
     printHead()
     selectedService = printMainMenuAndGetTheResponse()
     print(selectedService)
@@ -72,9 +112,13 @@ def printMenu():
     print("Key File Path" + keyFilePath)
 
 
-def startCloudAudit():
-    printMenu()
+def start():
+    while True:
+        startCloudAudit()
+        res = input("Do you want to audit more services? (y/n)")
+        if str(res).lower() == "n":
+            break
 
 
 if __name__ == '__main__':
-    startCloudAudit()
+    start()
